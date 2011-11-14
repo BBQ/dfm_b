953316220c18309b067a5589e70ab9dbbc532f5b
BBQ <iny@nm.ru>
2011-11-14 16:06:09 +0300

    api, find or create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   
-  attr_accessible :email, :password, :password_confirmation, :authentications_attributes
+  attr_accessible :facebook_id, :name, :email, :password, :password_confirmation, :authentications_attributes
   authenticates_with_sorcery! do |config|
     config.authentications_class = Authentication
   end
@@ -8,8 +8,8 @@ class User < ActiveRecord::Base
   has_many :authentications, :dependent => :destroy
   accepts_nested_attributes_for :authentications
 
-  validates_confirmation_of :password
-  validates_presence_of :password, :on => :create
+  # validates_confirmation_of :password
+  # validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
   
@@ -21,15 +21,20 @@ class User < ActiveRecord::Base
   end
   
   def get_user_by_fb_token(access_token)
-    rest = Koala::Facebook::GraphAndRestAPI.new(access_token) # pre-1.2beta
-    result = rest.get_object("me")
-    unless id = User.find_by_facebook_id(result["id"]).id
-      data[:email] = result["id"]
-      data[:name] = result["name"]
-      data[:id] = result["id"]
-      User.create(data).id
-      User.new.get_user_fb_friends(access_token)
+    begin
+      rest = Koala::Facebook::GraphAndRestAPI.new(access_token) # pre-1.2beta
+      result = rest.get_object("me")
+      if user = User.find_by_facebook_id(result["id"])
+        id = user.id
+      elsif result["email"] 
+        data = {:email => result["email"] , :name => result["name"], :facebook_id => result["id"]}
+        id = User.create(data)
+        User.new.get_user_fb_friends(access_token)
+      end
+    rescue
+      nil
     end
+    id
   end
   
   def get_user_fb_token(code)
@@ -38,14 +43,18 @@ class User < ActiveRecord::Base
     callback_url = Dishfm::Application.config.sorcery.facebook.callback_url
     
     oauth = Koala::Facebook::OAuth.new(key, secret, callback_url)
-    access_token = oauth.get_access_token(code)
+    begin
+      access_token = oauth.get_access_token(code)
+    rescue
+      nil
+    end
   end
   
   def get_user_fb_friends(code_or_access_token)
     if code_or_access_token
-      if access_token = User.new.get_user_fb_token(code)
-        access_token
-      elsif rest = Koala::Facebook::GraphAndRestAPI.new(access_token)
+      if User.new.get_user_fb_token(code_or_access_token)
+        access_token = User.new.get_user_fb_token(code_or_access_token)
+      else
         access_token = code_or_access_token         
       end
     end
