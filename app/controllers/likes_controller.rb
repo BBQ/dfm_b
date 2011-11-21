1977392c8b2342bd3cba63dd3e5ff6615f06dab3
BBQ <iny@nm.ru>
2011-11-21 20:35:44 +0300

    like debug

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,9 @@
 class LikesController < ApplicationController
   def add
     if current_user && params[:id]
+      like = Like.new.unlike?(current_user.id, params[:id])
+      @like = like if like
       @review = Like.new.save_me(current_user.id, params[:id])
-      @cu = current_user
     end
   end
 end
