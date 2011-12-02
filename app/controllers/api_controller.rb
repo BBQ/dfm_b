99cb922a63eb6c0c8a55efe958e07a9f68955ea9
BBQ <iny@nm.ru>
2011-12-02 14:20:42 +0300

    api fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -118,7 +118,7 @@ class ApiController < ApplicationController
     end
     
     restaurants = restaurants.where("LOWER(name) REGEXP '[[:<:]]#{params[:search].downcase}'") unless params[:search].blank?
-    restaurants = restaurants.where("restaurant.id IN (SELECT restaurant_id FROM dishes WHERE restaurant_id != 0 AND `name` LIKE '#{params[:keyword].downcase}%')") unless params[:keyword].blank?
+    restaurants = restaurants.where("restaurants.id IN (SELECT restaurant_id FROM dishes WHERE restaurant_id != 0 AND `name` LIKE '#{params[:keyword].downcase}%')") unless params[:keyword].blank?
     restaurants = restaurants.where(all_filters) unless all_filters.blank?
     count ||= restaurants.count
     restaurants = restaurants.limit("#{offset}, #{limit}")
