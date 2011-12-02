a205a103272a2d0a4d4273ca1df80b74a5e504eb
BBQ <iny@nm.ru>
2011-12-02 13:59:00 +0300

    api fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -107,7 +107,7 @@ class ApiController < ApplicationController
     
     if params[:lat] && params[:lon] # && params[:radius].to_f.to_s == params[:radius].to_s
       if params[:sort] == 'rating'
-        restaurants = Restaurant.near(params[:lat], params[:lon], params[:radius]).includes(:network).order("networks.rating/networks.votes DESC, networks.votes DESC")
+        restaurants = Restaurant.near(params[:lat], params[:lon], params[:radius]).includes(:network).order("networks.rating/networks.votes DESC, networks.votes DESC").group('restaurants.name')
       else
         restaurants = Restaurant.where('lat IS NOT NULL AND lon IS NOT NULL').by_distance(params[:lat], params[:lon])
       end    
