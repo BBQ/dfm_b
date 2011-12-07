984e5e889661b8a8d6b4e3ffc9501f018b7c3599
BBQ <iny@nm.ru>
2011-12-07 20:10:15 +0300

    api updated

--- app/models/like.rb
+++ app/models/like.rb
@@ -17,8 +17,8 @@ class Like < ActiveRecord::Base
         review.count_likes += 1 
         review.save
         like_id = review.likes.create({:user_id => user_id, :review_id => review_id}).id
-        hours_3 = Notification.where('user_id =? AND like_id = ? AND created_at >= current_time()-7', review.user.id, like_id)          
-        if 3hours_ago.blank?
+        hours_7 = Notification.where('user_id =? AND like_id = ? AND created_at >= current_time()-7', review.user.id, like_id)          
+        if hours_7.blank?
           if UserMailer.notification_email(user_id, review, 'review').deliver
             Notification.create({:user_id =>review.user.id, :like_id => like_id})
           end
