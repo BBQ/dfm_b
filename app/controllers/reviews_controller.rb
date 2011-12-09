f7c7c245e34cf6586c0251e763db19da13ba4793
BBQ <iny@nm.ru>
2011-12-09 16:31:52 +0300

    del review fix

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -68,16 +68,16 @@ class ReviewsController < ApplicationController
               
         if review && dish && restaurant && network
                
-               # if dish.dish_type_id == 9 && dish.votes == 0 
-               #               dish.delete
-               #               data[:deleted] = 'yes'
-               #             else
-               #               dish.save
-               #             end
-               #   
-               #    restaurant.save
-               #    network.save
-               #    review.destroy
+               if dish.dish_type_id == 9 && dish.votes == 0 
+                             dish.delete
+                             data[:deleted] = 'yes'
+                           else
+                             dish.save
+                           end
+                 
+                  restaurant.save
+                  network.save
+                  review.destroy
                
                result = "review with id #{params[:id]} gone forever!"
              end
