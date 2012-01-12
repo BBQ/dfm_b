54288f2d0f703b7d3d80ec1bfba828bafebd0a50
BBQ <iny@nm.ru>
2012-01-12 16:21:42 +0300

    api update

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -16,7 +16,7 @@ class Dish < ActiveRecord::Base
   mount_uploader :photo, ImageUploader
   
   def find_image
-    if photo.nil? || photo.blank?
+    if photo.to_s == ''
       review = Review.where("dish_id = ? AND photo IS NOT NULL", id).order('count_likes DESC').first
       review.photo if review
     else
