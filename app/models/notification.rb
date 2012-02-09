c31fa1c2ffb27faaaa253b259b84eda13fb7541d
BBQ <iny@nm.ru>
2012-02-09 13:47:54 +0300

    api fix

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,19 @@
 class Notification < ActiveRecord::Base
+  
+  def send_review_like_push(from_user_id, review)
+    
+    if device = APN::Device.where(:user_id => review.user.id).first  
+      if user = User.select(:name).find_by_id(from_user_id)  
+        notification = APN::Notification.new   
+        notification.device = device   
+        notification.badge = 1   
+        notification.sound = true   
+        notification.alert = "User #{user.name} like your review on dish #{review.dish.name}"   
+        notification.save
+        system "rake apn:notifications:deliver &"
+      end
+    end  
+    
+  end
+  
 end
