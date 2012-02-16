f548d17193022af9d3724890e4fb013e92d201f5
BBQ <iny@nm.ru>
2012-02-16 19:07:31 +0300

    api fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -83,7 +83,7 @@ class ApiController < ApplicationController
             data.push({
               :id => 0,
               :name => client.user.name,
-              :photo => client.user.photo
+              :photo => client.user.avatar
             })
           end
         end         
