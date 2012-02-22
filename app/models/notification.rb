e83201ee463e1f197eb38f610e49c2a346a9340f
BBQ <iny@nm.ru>
2012-02-22 13:32:02 +0300

    api update

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,26 +1,28 @@
 class Notification < ActiveRecord::Base
   
-  def self.send_push(from_user_id, review, type)
+  def self.send_push(from_user_id, data, type)
     
-    if device = APN::Device.where(:user_id => review.user.id).first  
-      if user = User.select(:name).find_by_id(from_user_id)  
-        
-        text = "#{type} your review" if type == 'like' || type = 'comment'
-        text = "started #{type} you" if type == 'following'
-        
-        alert = "#{user.name.split.first} #{user.name.split.second[0]}. #{text} #{review.dish.name}"
-        alert = "#{alert.slice 0 .. 40}..." if alert.length > 40
-        
-        notification = APN::Notification.new   
-        notification.device = device   
-        notification.badge = 1   
-        notification.sound = true   
-        notification.alert = alert    
-        notification.save
-        system "rake apn:notifications:deliver &"
+    if user = User.select(:name).find_by_id(from_user_id)  
+      
+      if type == 'like' || type = 'comment'
+        if device = APN::Device.where(:user_id => data.user.id).first   
+          alert = "#{user.name.split.first} #{user.name.split.second[0]}. #{type} your review #{data.dish.name}"
+        end
+      elsif type == 'following'
+        if device = APN::Device.where(:user_id => data).first  
+          alert = "#{user.name.split.first} #{user.name.split.second[0]}. started #{type} you"
+        end
       end
-    end  
-    
+      alert = "#{alert.slice 0 .. 40}..." if alert.length > 40
+
+      notification = APN::Notification.new   
+      notification.device = device   
+      notification.badge = 1   
+      notification.sound = true   
+      notification.alert = alert    
+      notification.save
+      system "rake apn:notifications:deliver &"
+    end
   end
   
 end
