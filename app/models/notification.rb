07f1fa8e6293e29d0105acdd31c14c22a0e0a624
BBQ <iny@nm.ru>
2012-03-14 16:41:53 +0300

    api fix

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,11 +1,11 @@
 class Notification < ActiveRecord::Base
   
-  def self.send_push(from_user_id, data, type)
+  def self.send_push(from_user_id, data, notification_type)
     
     if user = User.select(:name).find_by_id(from_user_id)  
       
       user_id_to = []
-      if (type == 'like' || type == 'comment') && user.id != data.user.id 
+      if (notification_type == 'like' || notification_type == 'comment') && user.id != data.user.id 
         if device = APN::Device.where(:user_id => data.user.id).first
           dish_name = data.home_cooked == true ? data.home_cook.name : data.dish.name
           alert = "#{user.name.split.first} #{user.name.split.second[0]}. #{type} your review #{dish_name}"
@@ -13,17 +13,17 @@ class Notification < ActiveRecord::Base
           user_id_to.push(data.user.id)
           review_id = data.id
         end
-      elsif type == 'comment_on_comment' && user.id != data.user.id 
+      elsif notification_type == 'comment_on_comment' && user.id != data.user.id 
         Comment.select(:user_id).where(:review_id => data.id).each do |c|
           if device = APN::Device.where(:user_id => c.user_id).first
             dish_name = data.home_cooked == true ? data.home_cook.name : data.dish.name
-            alert = "#{user.name.split.first} #{user.name.split.second[0]}. #{type} also commented on #{dish_name}"
+            alert = "#{user.name.split.first} #{user.name.split.second[0]}. also commented on #{dish_name}"
             badge = APN::Notification.where("user_id_to = ? and `read` != 1", data.user.id).count(:id)
             user_id_to.push(data.user.id)
             review_id = data.id
           end
         end
-      elsif type == 'dishin'
+      elsif notification_type == 'dishin'
         Follower.select(:user_id).where(:follow_user_id => from_user_id).each do |f|
           if device = APN::Device.where(:user_id => f.user_id).first
             dish_name = data.home_cooked == true ? data.home_cook.name : data.dish.name
@@ -33,13 +33,13 @@ class Notification < ActiveRecord::Base
             review_id = data.id
           end
         end          
-      elsif type == 'following' && user.id != data 
+      elsif notification_type == 'following' && user.id != data 
         if device = APN::Device.where(:user_id => data).first
-          alert = "#{user.name.split.first} #{user.name.split.second[0]}. started #{type} you"
+          alert = "#{user.name.split.first} #{user.name.split.second[0]}. started following you"
           badge = APN::Notification.where("user_id_to = ? and `read` != 1", data).count(:id)
           user_id_to.push(data)
         end
-      elsif type == 'tagged'
+      elsif notification_type == 'tagged'
         data.friends.split(',').each do |t|
           if device = APN::Device.where(:user_id => t).first
             alert = "tagged you at #{data.restaurant.name}"
@@ -47,7 +47,7 @@ class Notification < ActiveRecord::Base
             user_id_to.push(data)
           end
         end
-      elsif type == 'tagged_by_friend'
+      elsif notification_type == 'tagged_by_friend'
         data.friends.split(',').each do |t|
           if tagged = User.find_by_id(t)
             Follower.select(:user_id).where(:follow_user_id => tagged.id).each do |f|
@@ -59,7 +59,7 @@ class Notification < ActiveRecord::Base
             end
           end
         end      
-      elsif type == 'new_fb_user' && user.id != data 
+      elsif notification_type == 'new_fb_user' && user.id != data 
         if device = APN::Device.where(:user_id => data).first
           alert = "Your facebook friend #{user.name.split.first} #{user.name.split.second[0]}. has joined Dish.fm"
           badge = APN::Notification.where("user_id_to = ? and `read` != 1", data).count(:id)
@@ -75,7 +75,7 @@ class Notification < ActiveRecord::Base
             notification.badge = badge.to_i + 1   
             notification.sound = true   
             notification.alert = alert
-            notification.type = type
+            notification.notification_type = notification_type
             notification.review_id = review_id ? review_id : 0
             notification.user_id_from = from_user_id
             notification.user_id_to = user_id_to
--- db/migrate/20120314122503_add_type_and_review_id_to_apn_notifications.rb
+++ db/migrate/20120314122503_add_type_and_review_id_to_apn_notifications.rb
@@ -1,11 +1,11 @@
 class AddTypeAndReviewIdToApnNotifications < ActiveRecord::Migration
   def self.up
-    add_column :apn_notifications, :type, :string
+    add_column :apn_notifications, :notification_type, :string
     add_column :apn_notifications, :review_id, INT_UNSIGNED
   end
 
   def self.down
     remove_column :apn_notifications, :review_id
-    remove_column :apn_notifications, :type
+    remove_column :apn_notifications, :notification_type
   end
 end
