bdecc7895d464d861fb0549b41ad4863586ec8c9
BBQ <iny@nm.ru>
2012-03-20 17:47:34 +0300

    api update

--- app/models/dish_category.rb
+++ app/models/dish_category.rb
@@ -2,7 +2,7 @@ class DishCategory < ActiveRecord::Base
   has_many :dishes
   has_many :dish_deliveries
   
-  def get_id(name)
+  def self.get_id(name)
     unless id = find_by_name(name)
       id = create(:name => name).id
     end
