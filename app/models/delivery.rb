41c40fb248d7f7b8f296f4f07a344baa66fbd9cc
BBQ <iny@nm.ru>
2012-03-21 17:01:57 +0300

    api fix

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,5 @@
 class Delivery < ActiveRecord::Base
-  has_many :dish_deliveries, :foreign_key => :dish_category_id
-  has_many :reviews, :foreign_key => :restaurant_id 
+  has_many :dish_deliveries
   
   has_many :deliveries_tags, :dependent => :destroy
   has_many :tags, :through => :deliveries_tags
--- app/models/dish_delivery.rb
+++ app/models/dish_delivery.rb
@@ -1,9 +1,9 @@
 class DishDelivery < ActiveRecord::Base
   
-  belongs_to :dish_category, :foreign_key => :id
-  belongs_to :dish_type, :foreign_key => :dish_id
-  belongs_to :dish_subtype, :foreign_key => :dish_id
-  belongs_to :dish_extratype, :foreign_key => :dish_id  
+  belongs_to :dish_category, :foreign_key => :dish_category_id
+  belongs_to :dish_type, :foreign_key => :dish_type_id
+  belongs_to :dish_subtype, :foreign_key => :dish_subtype_id
+  belongs_to :dish_extratype, :foreign_key => :dish_extratype_id  
   
   has_many :reviews, :dependent => :destroy, :foreign_key => :dish_id
   has_many :dish_delivery_likes, :dependent => :destroy
