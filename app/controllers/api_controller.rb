6232bc6a1e65e7b5d6cdda320bbe89efc339d62b
BBQ <iny@nm.ru>
2012-03-21 16:38:13 +0300

    api fix

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1001,7 +1001,7 @@ class ApiController < ApplicationController
         types = []
       
         dishes.group(:dish_category_id).each do |dish|
-          sort = DishCategoryOrder.find_by_restaurant_id_and_dish_category_id(restaurant.id, dish.dish_category.id)
+          sort = DishCategoryOrder.find_by_restaurant_id_and_dish_category_id(restaurant.id, dish.dish_category_id)
           categories.push({
             :id => dish.dish_category.id, 
             :name => dish.dish_category.name_eng.nil? ? dish.dish_category.name : dish.dish_category.name_eng, 
