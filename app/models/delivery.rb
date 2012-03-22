e66f369999658ed2c8c2ed27ff767cafd7b8883c
BBQ <iny@nm.ru>
2012-03-22 20:43:53 +0300

    api fix

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -1,6 +1,6 @@
 class Delivery < ActiveRecord::Base
   has_many :dish_deliveries
-  has_many :reviews
+  has_many :reviews, :foreign_key => :restaurant_id
   
   has_many :deliveries_tags, :dependent => :destroy
   has_many :tags, :through => :deliveries_tags
