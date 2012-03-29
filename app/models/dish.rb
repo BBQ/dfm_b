1a53185ca9f119b66d46b56208a8b16a916b4a9d
BBQ <iny@nm.ru>
2012-03-29 17:49:22 +0400

    api update

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -18,14 +18,6 @@ class Dish < ActiveRecord::Base
       
   mount_uploader :photo, ImageUploader
   
-  def as_json(options={})
-    self[:address] = "#{address}, #{city}" unless city.nil?
-    self[:rating] = self.network.rating
-    self[:votes] = self.network.votes
-    self[:fsq_id] = self.fsq_id || ''        
-    super(:only => [:id, :name, :address, :rating, :votes, :lat, :lon, :network_id, :fsq_id, :fsq_checkins_count], :methods => [:has_menu, :thumb, :categories])
-  end
-  
   def self_review
     
     get_likes = []
