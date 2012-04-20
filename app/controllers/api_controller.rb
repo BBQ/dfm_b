3f9907574b82aee8d08010cbca313ea251be766e
BBQ <iny@nm.ru>
2012-04-20 17:52:07 +0400

    fix apn gem

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -345,16 +345,14 @@ class ApiController < ApplicationController
       end
       
       #Add push token
-      if params[:push_token] && session
+      if params[:push_token] && session && !APN::Device.find_by_token_and_user_id(params[:push_token], session[:user_id])
         if push_token = APN::Device.find_by_token(params[:push_token])
           if push_token.user_id == 0
             push_token.user_id = session[:user_id]
             push_token.save
-          elsif push_token.user_id != session[:user_id].to_i
-            APN::Device.create({:token => params[:push_token], :user_id => session[:user_id]})
           end
         else
-          APN::Device.create({:token => params[:push_token], :user_id => session[:user_id]})
+          APN::Device.create(:token => params[:push_token], :user_id => session[:user_id])
         end
       end
       user_preferences = UserPreference.for_user.find_by_user_id session[:user_id] if session
