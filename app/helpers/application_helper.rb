bf4b59c719f8e492bb6f308ab0ef159c6a8bd141
BBQ <iny@nm.ru>
2012-04-25 18:19:21 +0400

    ylp upd

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   def fb_opengraph_meta_tag(action=nil)
     raw %Q{<meta property="fb:app_id" content="#{Rails.application.config.sorcery.facebook.key}" />
-    <meta property="og:type" content="dish_fm:comment" />
+    <meta property="og:type" content="dish_fm:review" />
     <meta property="og:title" content="Stuffed Cookies" />
     <meta property="og:image" content="http://fbwerks.com:8000/zhen/cookie.jpg" />
     <meta property="og:description" content="The Turducken of Cookies" />
