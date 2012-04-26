e12ede065bd220a790d746e6138f40e5f5fc994b
BBQ <iny@nm.ru>
2012-04-26 16:40:37 +0400

    api upd

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -417,8 +417,8 @@ class ApiController < ApplicationController
           :restaurant_categories => timestamp ? rc.where('updated_at >= ?', timestamp) : rc.all,
           # :cities => timestamp ? locations.where('updated_at >= ?', timestamp) : locations.all,
           :tags => Tag.get_all(timestamp),
-          :error => $error,
           :force_logout => 1
+          :error => $error,
     }
   end
   
