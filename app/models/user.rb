d8aee9727de0f79f5159129c18d096e89c29bdea
BBQ <iny@nm.ru>
2012-05-02 16:32:40 +0400

    api upd

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   end
   
   has_many :authentications, :dependent => :destroy
-  belongs_to :user_preference, :dependent => :destroy
+  has_one :user_preference, :dependent => :destroy
   accepts_nested_attributes_for :authentications
 
   # validates_confirmation_of :password
