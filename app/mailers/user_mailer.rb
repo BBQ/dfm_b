1d403aa4c9f5aa8856cdef759ec505669e879cd3
BBQ <iny@nm.ru>
2012-05-04 16:18:31 +0400

    api upd

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,10 @@
 class UserMailer < ActionMailer::Base
   
-  default :from => 
+  default :from => "Dish.FM <hi@dish.fm>"
    
   def email_notification
     @url  = "http://test.dish.fm"
     subject = "Dish.fm Notifications"
-    from = "Dish.fm <hi@dish.fm>"
     
     APN::Notification.where("mailed_at IS NULL").each do |n|
       if user_to = User.find_by_id(n.user_id_to)
@@ -13,7 +12,7 @@ class UserMailer < ActionMailer::Base
         if email = user_to.email
           @user = user_to.name
           @text = "#{User.find_by_id(n.user_id_from).name.split(' ')[0]} #{n.alert.downcase}"
-          mail(:to => email, :subject => subject, :from => from)
+          mail(:to => email, :subject => subject, :from => 'hi@dish.fm')
           
           n.mailed_at = Time.now
           n.save
