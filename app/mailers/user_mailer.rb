02573a028db793e66b556b373d12f33104164c64
BBQ <iny@nm.ru>
2012-05-04 16:42:08 +0400

    api upd

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,6 @@
 class UserMailer < ActionMailer::Base
   
-  default :from => "no-reply@example.com"
+  default :from => "hi@dish.fm"
    
   def email_notification
     @url  = "http://test.dish.fm"
