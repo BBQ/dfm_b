821c1c338e7ab9dcdd13c0b9fb9f3ca6dba10e86
BBQ <iny@nm.ru>
2012-05-29 18:08:04 +0400

    css update landing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Dishfm::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  Sass::Plugin.options[:never_update] = true
 end
