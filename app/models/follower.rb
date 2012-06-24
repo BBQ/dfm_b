f14c24ab9894f23f0d12896b5d8bea2345b20869
BBQ <iny@nm.ru>
2012-06-24 18:01:50 +0400

    fb follow action

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,12 +1,6 @@
 class Follower < ActiveRecord::Base
   validates :user_id, :uniqueness => {:scope => :follow_user_id}
   
-  after_create :post_on_facebook
-
-  def post_on_facebook(record)
-    system "rake facebook:follow ID='#{record.id}' &"
-  end
-  
   def destroy
     if n = APN::Notification.find_by_user_id_from_and_user_id_to_and_notification_type(user_id,follow_user_id,'following')
       n.destroy
