99f1a27eba42361ca1b29ed7d78180460eecb559
BBQ <iny@nm.ru>
2012-10-17 14:20:51 +0400

    review map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,7 +2,7 @@ class MapController < ApplicationController
   
   def index
     @markers = []
-    reviews = Review.select([:lat, :lng]).where("lat > 55 && lat < 56 && lng > 37 && lng < 38").each { |r| @markers.push("['#{r.lat}, #{r.lng}', #{r.lat}, #{r.lng}, 1]")}
+    reviews = Review.select([:lat, :lng]).where("lat > 55.5 && lat < 55.9 && lng > 37.35 && lng < 38").each { |r| @markers.push("['#{r.lat}, #{r.lng}', #{r.lat}, #{r.lng}, 1]")}
     @markers = "[#{@markers.join(',')}]"
   end
   
