1502be836c977ec60be75a469d89582bad57fdd9
BBQ <iny@nm.ru>
2012-10-17 14:00:23 +0400

    review map

---
+++ app/controllers/map_controller.rb
@@ -0,0 +1,10 @@
+class MapController < ApplicationController
+  
+  def index
+    @markers = []
+    
+    reviews = Review.select([:lat, :lng]).each { |r| @markers.push("['review', #{r.lat}, #{r.lng}, 1]")}
+    @markers = "[#{@markers.join(',')}]"
+  end
+  
+end
